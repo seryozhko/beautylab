f3967f153ae3176407f035bfd74647fb9757e30b
Alexsey Seryozhko <admin@prime5.ru>
2018-02-06 02:08:39 +0300

    Firebase init

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const sass = require('gulp-sass');
 
 gulp.task('sass', () => gulp.src(['src/scss/*.scss'])
   .pipe(sass())
-  .pipe(gulp.dest('src/css'))
+  .pipe(gulp.dest('public/css'))
   .pipe(browserSync.stream()));
 
 gulp.task('serve', () => {
-  browserSync.init({ server: './src' });
+  browserSync.init({ server: './public' });
   gulp.watch('src/scss/*.scss').on('change', gulp.series('sass', browserSync.reload));
-  gulp.watch('src/*.html').on('change', gulp.series('sass', browserSync.reload));
+  gulp.watch('public/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', gulp.series('sass', 'serve'));
